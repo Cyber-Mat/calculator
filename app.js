3d2a7288ce63da35201cb7a6907af55993c9f708
cyber-mat <olaniyanolamide@gmail.com>
2020-06-24 21:42:43 +0100

    Keyboard support

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const modelController = (() => {
 
   return {
     storeValue: (value) => {
-      if (value === '=' || value === 'C' || value === 'DEL') {
+      if (value === '=' || value === 'C') {
         return;
       } else if (value === 'x') {
         data.allItems.push('*');
@@ -67,6 +67,9 @@ const modelController = (() => {
         // return data.allItems[data.allItems.length - 1];
       } else if (value === '%') {
         data.allItems = [`${data.allItems.join('')} / 100`];
+      } else if (value === 'DEL') {
+        data.allItems.pop();
+        return data.allItems;
       } else {
         data.allItems.push(value);
       }
@@ -76,7 +79,7 @@ const modelController = (() => {
       return data.allItems;
     },
 
-    execute: (dom) => {
+    execute: () => {
       try {
         let forData = data.allItems.map((c, i, a) => {
           if (c === '*' && a[i + 1] === '*') {
@@ -129,6 +132,11 @@ const modelController = (() => {
       return data.allItems;
     },
 
+    retainData: (total) => {
+      data.allItems.push(total);
+      return data.allItems;
+    },
+
     resetTotal: () => {
       data.total = 0;
       return data.total;
@@ -152,20 +160,23 @@ const appController = ((viewCtrl, modelCtrl) => {
     value = e.target.textContent;
 
     if (e.target.className === 'calc__btn calc__btn--equal') {
-      modelCtrl.execute(dom);
+      modelCtrl.execute();
       total = modelCtrl.returnTotal();
       modelCtrl.resetData();
-      modelCtrl.resetTotal();
+      modelCtrl.retainData(total);
+      viewCtrl.renderTotal(total);
+
+      //modelCtrl.resetData();
     } else if (e.target.className === 'calc__btn calc__btn--clear') {
       modelCtrl.resetData();
       modelCtrl.resetTotal();
       viewCtrl.clearDisplay();
+    } else {
+      modelCtrl.storeValue(value);
+      dataArr = modelCtrl.returnData();
+      viewCtrl.renderInput(dataArr);
+      viewCtrl.renderTotal(total);
     }
-
-    modelCtrl.storeValue(value);
-    dataArr = modelCtrl.returnData();
-    viewCtrl.renderInput(dataArr);
-    viewCtrl.renderTotal(total);
   };
 
   const init = () => {
@@ -179,5 +190,75 @@ const appController = ((viewCtrl, modelCtrl) => {
 
   // Event Listener
   allBtnArr.forEach((arr) => arr.addEventListener('click', calculate));
-  //allBtnArr.forEach((arr) => arr.addEventListener('click', (e) => console.log(e.target.firstElementChild.textContent)));
+  window.addEventListener('keypress', (e) => {
+    let value, dataArr, total;
+    //console.log(e.keyCode);
+    e.preventDefault();
+
+    if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.which >= 48 && e.which <= 57)) {
+      if (e.keyCode === 48 || e.which === 48) {
+        value = 0;
+      } else if (e.keyCode === 49 || e.which === 49) {
+        value = 1;
+      } else if (e.keyCode === 50 || e.which === 50) {
+        value = 2;
+      } else if (e.keyCode === 51 || e.which === 51) {
+        value = 3;
+      } else if (e.keyCode === 52 || e.which === 52) {
+        value = 4;
+      } else if (e.keyCode === 53 || e.which === 53) {
+        value = 5;
+      } else if (e.keyCode === 54 || e.which === 54) {
+        value = 6;
+      } else if (e.keyCode === 55 || e.which === 55) {
+        value = 7;
+      } else if (e.keyCode === 56 || e.which === 56) {
+        value = 8;
+      } else if (e.keyCode === 57 || e.which === 57) {
+        value = 9;
+      }
+    }
+
+    if (e.keyCode === 37 || e.which === 37) {
+      value = '%';
+    } else if (e.keyCode === 42 || e.keyCode === 120 || e.which === 42 || e.which === 120 || e.keyCode === 88 || e.which === 88) {
+      value = 'x';
+    } else if (e.keyCode === 43 || e.which === 43) {
+      value = '+';
+    } else if (e.keyCode === 45 || e.which === 45) {
+      value = '-';
+    } else if (e.keyCode === 47 || e.keyCode === 47) {
+      value = '÷';
+    } else if (e.keyCode === 46 || e.keyCode === 46) {
+      value = '.';
+    }
+
+    if (e.keyCode === 13 || e.keyCode === 61 || e.which === 13 || e.which === 61) {
+      modelCtrl.execute();
+      total = modelCtrl.returnTotal();
+      modelCtrl.resetData();
+      modelCtrl.retainData(total);
+      viewCtrl.renderTotal(total);
+    } else {
+      modelCtrl.storeValue(value);
+      dataArr = modelCtrl.returnData();
+      viewCtrl.renderInput(dataArr);
+      viewCtrl.renderTotal(total);
+    }
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if (e.keyCode === 8) {
+      let value, dataArr, total;
+
+      e.preventDefault();
+
+      value = 'DEL';
+
+      modelCtrl.storeValue(value);
+      dataArr = modelCtrl.returnData();
+      viewCtrl.renderInput(dataArr);
+      viewCtrl.renderTotal(total);
+    }
+  });
 })(viewController, modelController);
