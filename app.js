f96c97bd4ebbd147cf508f21384ec03750c6b308
cyber-mat <olaniyanolamide@gmail.com>
2020-06-24 11:43:55 +0100

    App now rendering input and result

--- app.js
+++ app.js
@@ -10,12 +10,26 @@
 *************************************************************/
 
 const viewController = (() => {
+  const dom = {
+    screenBottom: '.calc__screen--bottom',
+    screenTop: '.calc__screen--top',
+    allBtn: '.calc__btn',
+    clearBtn: '.btn__clear',
+  };
+
   return {
-    dom: {
-      screenBottom: '.calc__screen--bottom',
-      screenTop: '.calc__screen--top',
-      allBtn: '.calc__btn',
-      clearBtn: '.btn__clear',
+    dom,
+    renderInput: (arr) => {
+      document.querySelector(dom.screenTop).textContent = arr.join(' ');
+    },
+
+    renderTotal: (total) => {
+      document.querySelector(dom.screenBottom).textContent = total;
+    },
+
+    clearDisplay: () => {
+      document.querySelector(dom.screenTop).textContent = '';
+      document.querySelector(dom.screenBottom).textContent = '0';
     },
   };
 })();
@@ -32,7 +46,7 @@ const modelController = (() => {
 
   return {
     storeValue: (value) => {
-      if (value === '=') {
+      if (value === '=' || value === 'C' || value === 'DEL') {
         return;
       } else if (value === 'x') {
         data.allItems.push('*');
@@ -41,15 +55,44 @@ const modelController = (() => {
       }
     },
 
-    execute: () => {
-      const dataStr = eval(data.allItems.join(''));
-      console.log(dataStr);
+    returnData: () => {
+      return data.allItems;
     },
+
+    execute: () => {
+      const dataStr = Function(`"use strict"; return ${data.allItems.join('')}`)();
+      data.total = dataStr;
+    },
+
+    returnTotal: () => {
+      let total = data.total;
+
+      if (!Number.isInteger(total)) {
+        [int, float] = total.toString().split('.');
+        float = float.toString().split('');
+
+        let formattedFloat = [];
+
+        if (float.length > 10) {
+          for (let i = 0; i < 10; i++) {
+            formattedFloat.push(float[i]);
+          }
+
+          total = parseFloat(`${int.toString()}.${formattedFloat.join('')}`);
+          return total;
+        } else {
+          return total;
+        }
+      }
+      return total;
+    },
+
     resetData: () => {
       data.allItems = [];
       //console.log(data.allItems);
       return data.allItems;
     },
+
     test: () => data.allItems,
   };
 })();
@@ -64,23 +107,45 @@ const appController = ((viewCtrl, modelCtrl) => {
 
   // Calculate Function
   const calculate = (e) => {
-    let value;
+    let value, dataArr, total;
     if (e.target.firstElementChild) {
       value = e.target.firstElementChild.textContent;
 
       if (e.target.firstElementChild.className === 'btn__equal') {
         modelCtrl.execute();
+
+        total = modelCtrl.returnTotal();
+
         modelCtrl.resetData();
+      } else if (e.target.firstElementChild.className === 'btn__clear') {
+        modelCtrl.resetData();
+        viewCtrl.clearDisplay();
       }
     } else {
       value = e.target.textContent;
       if (e.target.className === 'btn__equal') {
         modelCtrl.execute();
+        total = modelCtrl.returnTotal();
         modelCtrl.resetData();
+      } else if (e.target.className === 'btn__clear') {
+        modelCtrl.resetData();
+        viewCtrl.clearDisplay();
       }
     }
     modelCtrl.storeValue(value);
+    dataArr = modelCtrl.returnData();
+    viewCtrl.renderInput(dataArr);
+    viewCtrl.renderTotal(total);
   };
+
+  const init = () => {
+    window.addEventListener('load', () => {
+      modelCtrl.resetData();
+      viewCtrl.clearDisplay();
+    });
+  };
+
+  init();
 
   // Event Listener
   allBtnArr.forEach((arr) => arr.addEventListener('click', calculate));
